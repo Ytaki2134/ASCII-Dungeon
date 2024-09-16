c12182a947f4366ed7987d436d6febd2a6de9a23
bielskialeksi <92392918+bielskialeksi@users.noreply.github.com>
2024-09-16 17:46:05 +0200

    Update StateMachine

---
+++ State.cpp
@@ -0,0 +1,13 @@
+#include "State.h"
+
+void State::OnEnter()
+{
+}
+
+void State::OnUpdate()
+{
+}
+
+void State::OnExit()
+{
+}
---
+++ State.h
@@ -0,0 +1,16 @@
+#pragma once
+class State
+{
+private:
+
+
+
+
+public:
+
+	void OnEnter();
+	void OnUpdate();
+	void OnExit();
+
+};
+
